3f1d491fbd84be4d7847ac27bd65fc4fed86136d
shlabisa <shlabisa@sarao.ac.za>
2025-06-05 15:40:33 +0200

    Added state var for controlling collapsable nav, and getting active route

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isNavbarCollapsed = false;
+  currentRoute: string = 'home';
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = this.activatedRoute.snapshot.url[0].path;
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  isActiveRoute(route: string): boolean {
+    return this.currentRoute === route;
+  }
 }
