dbe5fc3963b2b00eace9b615feda02e04bd1e65e
Slindile Nyide <105649291+slielondie@users.noreply.github.com>
2025-06-20 15:37:03 +0200

    updates main file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
+from fastapi import FastAPI, HTTPException
+from classes.user import User
+from endpoints.users import get_users, post_user
+import uvicorn
+
+BASE_URL = "/api/v1"
+app = FastAPI()
+
+# curl -X GET -H "Content-Type: application/json" -d '{}' "http://127.0.0.1:8000/api/v1/users"
+@app.get(f"{BASE_URL}/users", response_model=list[User])
+def get_users_endpoint():
+    return get_users()
+
+# curl -X POST -H "Content-Type: application/json" -d '{"email_address": "shlabisa@sarao.ac.za", "password": "hlabisa"}' "http://127.0.0.1:8000/api/v1/users"
+@app.post(f"{BASE_URL}/users", response_model=User)
+def post_users_endpoint(user_dict: dict):
+    return post_user(user_dict)
 
 def main() -> None:
-    pass
-
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+    
 if __name__ == "__main__":
     main()
