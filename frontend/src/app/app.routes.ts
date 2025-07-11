7c2b474efd4d9c62bb21c087b1790c9144b41c07
shlabisa <shlabisa@sarao.ac.za>
2025-07-11 16:44:18 +0200

    Added children routes for telescopes comp

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,66 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CommandsComponent } from './commands/commands.component';
-import { ReadingsComponent } from './readings/readings.component';
-import { CamComponent } from './cam/cam.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CommandsComponent } from './components/commands/commands.component';
+import { ReadingsComponent } from './components/readings/readings.component';
+import { CamComponent } from './components/cam/cam.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { TelescopesComponent } from './components/telescopes/telescopes.component';
+import { TelescopeDetailsComponent } from './components/telescope-details/telescope-details.component';
+import { UsersComponent } from './components/users/users.component';
+import { SourcesComponent } from './components/sources/sources.component';
+import { SourceDetailsComponent } from './components/source-details/source-details.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
 
 export const routes: Routes = [
-    {
-        path: "home", component: HomeComponent
+    { 
+        path: '', redirectTo: 'sign-in', pathMatch: 'full' 
+    },
+    { 
+        path: 'sign-in', component: SignInComponent 
     },
     {
-        path: "cam", component: CamComponent
-    },
-    {
-        path: "commands", component: CommandsComponent
-    },
-    {
-        path: "readings", component: ReadingsComponent
-    }
+        path: '', component: AppLayoutComponent, children: [
+            { 
+                path: 'telescopes', component: TelescopesComponent, children: [
+                    {
+                        path: ':id/details', component: TelescopeDetailsComponent
+                    },
+                    {
+                        path: ':id/cam', component: CamComponent
+                    },
+                    {
+                        path: ':id/commands', component: CommandsComponent
+                    },
+                    {
+                        path: ':id/readings', component: ReadingsComponent
+                    },
+                ]
+            },
+            { 
+                path: 'users', component: UsersComponent, children: [
+                    {
+                        path: ':id/details', component: UserDetailsComponent
+                    }
+                ]
+            },
+            {
+                path: 'sources', component: SourcesComponent, children: [
+                    {
+                        path: ':id/details', component: SourceDetailsComponent
+                    }
+                ]
+            }
+        ]
+    },/*
+    { 
+        path: '**', redirectTo: 'sign-in', pathMatch: 'full' 
+    }*/
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
