fdc741d6e40efedd0f42f7e71f52a44dc259e8c1
shlabisa <159113956+shlabisa@users.noreply.github.com>
2025-07-17 15:01:13 +0200

    Update main.py, added get, delete and put user enpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from classes.user import User
-from endpoints.users import get_users, post_user
+from endpoints.users import get_users, post_user, get_user, delete_user, update_user
 import uvicorn
 
 BASE_URL = "/api/v1"
@@ -16,8 +16,23 @@ def get_users_endpoint():
 def post_users_endpoint(user_dict: dict):
     return post_user(user_dict)
 
+# curl -X GET -H "Content-Type: application/json" -d '{}' "http://127.0.0.1:8000/api/v1/users/1"
+@app.get(f"{BASE_URL}/users/{{user_id}}", response_model=User)
+def get_user_endpoint(user_id: int):
+    return get_user(user_id)
+
+# curl -X DELETE -H "Content-Type: application/json" -d '{}' "http://127.0.0.1:8000/api/v1/users/1"
+@app.delete(f"{BASE_URL}/users/{{user_id}}", response_model=User)
+def delete_user_endpoint(user_id: int):
+    return delete_user(user_id)
+
+# curl -X PUT -H "Content-Type: application/json" -d '{"email_address": "new@email.com", "password": "newpassword"}' "http://127.0.0.1:8000/api/v1/users/1"
+@app.put(f"{BASE_URL}/users/{{user_id}}", response_model=User)
+def update_user_endpoint(user_id: int, user_dict: dict):
+    return update_user(user_id, user_dict)
+
 def main() -> None:
     uvicorn.run(app, host="127.0.0.1", port=8000)
-    
+
 if __name__ == "__main__":
     main()
