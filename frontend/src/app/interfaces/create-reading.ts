d980d2811a546335e537ef113e375f159ed7101b
shlabisa <shlabisa@sarao.ac.za>
2026-02-06 10:08:25 +0200

    Extend CreateReading interface with new attributes for magnetometer, gyroscope, accelerometer and gps

--- frontend/src/app/interfaces/create-reading.ts
+++ frontend/src/app/interfaces/create-reading.ts
@@ -4,4 +4,16 @@ export interface CreateReading {
     el_angle: number;
     health_status: string;
     movement_status: string;
+    mag_x: number;
+    mag_y: number;
+    mag_z: number;
+    acc_x: number;
+    acc_y: number;
+    acc_z: number;
+    gyo_x: number;
+    gyo_y: number;
+    gyo_z: number;
+    lon: number;
+    lat: number;
+    alt: number;
 }
