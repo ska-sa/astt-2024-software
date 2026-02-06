8ee9affcf7dcc5babe8b003bd54caa40e5366809
shlabisa <159113956+shlabisa@users.noreply.github.com>
2026-02-06 10:51:52 +0200

    Change uvicorn host to '0.0.0.0' for accessibility

--- backend/main.py
+++ backend/main.py
@@ -189,8 +189,7 @@ def update_position_endpoint(position_id: int, position: Position):
 
 
 def main() -> None:
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, workers=2, reload=True)
-    #uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
 
 if __name__ == "__main__":
     main()
