5ebc33b881bff97c94a600f0b6b69e1475064aee
Slindile <slindilenyide142@gmail.com>
2026-05-15 15:15:21 +0200

    Added source parameters

--- backend/endpoints/sources.py
+++ backend/endpoints/sources.py
@@ -9,15 +9,19 @@ def get_source(source_id: int) -> Source:
     db = Database()
     _, db_select_source_outputs = db.read(table_name, criteria={'id': source_id})
     if db_select_source_outputs:
-        id, name, initial_right_ascension, initial_declination, right_ascension_rate, declination_rate, reset_period_days, created_at = db_select_source_outputs[0]
+        id, name, m_1, m_2, c_1, c_2, T_ra, A, phi, D, T_dec, created_at = db_select_source_outputs[0]
         return Source(
             id=id,
             name=name,
-            initial_right_ascension=initial_right_ascension,
-            initial_declination=initial_declination,
-            right_ascension_rate=right_ascension_rate,
-            declination_rate=declination_rate,
-            reset_period_days=reset_period_days,
+            m_1=m_1,
+            m_2=m_2,
+            c_1=c_1,
+            c_2=c_2,
+            T_ra=T_ra,
+            A=A,
+            phi=phi,
+            D=D,
+            T_dec=T_dec,
             created_at=datetime.fromisoformat(created_at)
         )
     else:
@@ -28,15 +32,19 @@ def get_sources() -> list[Source]:
     _, db_select_source_outputs = db.read(table_name)
     sources = []
     for db_select_source_output in db_select_source_outputs:
-        id, name, initial_right_ascension, initial_declination, right_ascension_rate, declination_rate, reset_period_days, created_at = db_select_source_output
+        id, name, m_1, m_2, c_1, c_2, T_ra, A, phi, D, T_dec, created_at = db_select_source_output
         sources.append(Source(
             id=id,
             name=name,
-            initial_right_ascension=initial_right_ascension,
-            initial_declination=initial_declination,
-            right_ascension_rate=right_ascension_rate,
-            declination_rate=declination_rate,
-            reset_period_days=reset_period_days,
+            m_1=m_1,
+            m_2=m_2,
+            c_1=c_1,
+            c_2=c_2,
+            T_ra=T_ra,
+            A=A,
+            phi=phi,
+            D=D,
+            T_dec=T_dec,
             created_at=datetime.fromisoformat(created_at)
         ))
     return sources
@@ -45,15 +53,19 @@ def post_source(source: CreateSource) -> Source:
     db = Database()
     _, _ = db.insert(table_name, source.__dict__)
     _, db_select_source_outputs = db.read(table_name, criteria=source.__dict__)
-    id, name, initial_right_ascension, initial_declination, right_ascension_rate, declination_rate, reset_period_days, created_at = db_select_source_outputs[-1]
+    id, name, m_1, m_2, c_1, c_2, T_ra, A, phi, D, T_dec, created_at = db_select_source_outputs[-1]
     return Source(
         id=id,
         name=name,
-        initial_right_ascension=initial_right_ascension,
-        initial_declination=initial_declination,
-        right_ascension_rate=right_ascension_rate,
-        declination_rate=declination_rate,
-        reset_period_days=reset_period_days,
+        m_1=m_1,
+        m_2=m_2,
+        c_1=c_1,
+        c_2=c_2,
+        T_ra=T_ra,
+        A=A,
+        phi=phi,
+        D=D,
+        T_dec=T_dec,
         created_at=datetime.fromisoformat(created_at)
     )
 
@@ -61,15 +73,19 @@ def delete_source(source_id: int) -> Source:
     db = Database()
     _, db_select_source_outputs = db.read(table_name, criteria={'id': source_id})
     if db_select_source_outputs:
-        id, name, initial_right_ascension, initial_declination, right_ascension_rate, declination_rate, reset_period_days, created_at = db_select_source_outputs[0]
+        id, name, m_1, m_2, c_1, c_2, T_ra, A, phi, D, T_dec, created_at = db_select_source_outputs[0]
         source = Source(
             id=id,
             name=name,
-            initial_right_ascension=initial_right_ascension,
-            initial_declination=initial_declination,
-            right_ascension_rate=right_ascension_rate,
-            declination_rate=declination_rate,
-            reset_period_days=reset_period_days,
+            m_1=m_1,
+            m_2=m_2,
+            c_1=c_1,
+            c_2=c_2,
+            T_ra=T_ra,
+            A=A,
+            phi=phi,
+            D=D,
+            T_dec=T_dec,
             created_at=datetime.fromisoformat(created_at)
         )
         success, _ = db.delete(table_name, criteria={'id': source_id})
@@ -85,15 +101,19 @@ def update_source(source_id: int, source: Source) -> Source:
     success, _ = db.update(table_name, criteria={'id': source_id}, data=source.__dict__)
     if success:
         _, db_select_source_outputs = db.read(table_name, criteria={'id': source_id})
-        id, name, initial_right_ascension, initial_declination, right_ascension_rate, declination_rate, reset_period_days, created_at = db_select_source_outputs[0]
+        id, name, m_1, m_2, c_1, c_2, T_ra, A, phi, D, T_dec, created_at = db_select_source_outputs[0]
         return Source(
             id=id,
             name=name,
-            initial_right_ascension=initial_right_ascension,
-            initial_declination=initial_declination,
-            right_ascension_rate=right_ascension_rate,
-            declination_rate=declination_rate,
-            reset_period_days=reset_period_days,
+            m_1=m_1,
+            m_2=m_2,
+            c_1=c_1,
+            c_2=c_2,
+            T_ra=T_ra,
+            A=A,
+            phi=phi,
+            D=D,
+            T_dec=T_dec,
             created_at=datetime.fromisoformat(created_at)
         )
     else:
