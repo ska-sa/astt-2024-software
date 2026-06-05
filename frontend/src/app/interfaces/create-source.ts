3a3e0d67ab841a5014b3af046f41241f1391d30f
shlabisa <shlabisa@sarao.ac.za>
2026-06-05 15:48:21 +0200

    Add source interfaces

---
+++ frontend/src/app/interfaces/create-source.ts
@@ -0,0 +1,12 @@
+export interface CreateSource {
+    name: string;
+    m_1: number;
+    m_2: number;
+    c_1: number;
+    c_2: number;
+    T_ra: number;
+    A: number;
+    phi: number;
+    D: number;
+    T_dec: number;
+}
---
+++ frontend/src/app/interfaces/source.ts
@@ -0,0 +1,14 @@
+export interface Source {
+    id: number;
+    name: string;
+    m_1: number;
+    m_2: number;
+    c_1: number;
+    c_2: number;
+    T_ra: number;
+    A: number;
+    phi: number;
+    D: number;
+    T_dec: number;
+    created_at: string;
+}
