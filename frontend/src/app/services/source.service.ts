105c9fa20bf84b14d5c47959b19fa6fcef0b75ee
shlabisa <shlabisa@sarao.ac.za>
2026-06-12 17:13:28 +0200

    Add services for crud sources

--- frontend/src/app/services/source.service.ts
+++ frontend/src/app/services/source.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Source } from '../interfaces/source';
+import { CreateSource } from '../interfaces/create-source';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +20,17 @@ export class SourceService {
 
   getSources(): Observable<Source[]> {
     return this.httpClient.get<Source[]>(`${this.url}/sources`, { headers: this.httpHeaders });
+  }
+
+  postSource(createSource: CreateSource): Observable<Source> {
+    return this.httpClient.post<Source>(`${this.url}/sources`, createSource, { headers: this.httpHeaders });
+  }
+
+  updateSource(id: number, source: Source): Observable<Source> {
+    return this.httpClient.put<Source>(`${this.url}/sources/${id}`, source, { headers: this.httpHeaders });
+  }
+
+  deleteSource(id: number): Observable<Source> {
+    return this.httpClient.delete<Source>(`${this.url}/sources/${id}`, { headers: this.httpHeaders });
   }
 }
