0a93280f1e1ba62d18a6a94207bc9943866d70d3
Slindile <slindilenyide142@gmail.com>
2026-07-03 12:45:03 +0200

    Updated commands endpoints: Track and Point

--- backend/classes/command.py
+++ backend/classes/command.py
@@ -1,12 +1,48 @@
 from dataclasses import dataclass
+from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
+from .source import CreateSource
+
+class PointCommand(BaseModel):
+    target_az_angle: float
+    target_el_angle: float
+
+class TrackCommand(BaseModel):
+    source: CreateSource
 
 class CreateCommand(BaseModel):
     user_id: int
     telescope_id: int
-    target_az_angle: float
-    target_el_angle: float
+    command_type: str
+    track : Optional[TrackCommand] = None
+    point : Optional[PointCommand] = None
+
+    def to_dict(self) -> dict:
+        data = {
+            "user_id": self.user_id,
+            "telescope_id": self.telescope_id,
+            "command_type": self.command_type
+        }
+        if self.track:
+            data.update({
+                "name": self.track.source.name,
+                "m_1": self.track.source.m_1,
+                "m_2": self.track.source.m_2,
+                "c_1": self.track.source.c_1,
+                "c_2": self.track.source.c_2,
+                "T_ra": self.track.source.T_ra,
+                "A": self.track.source.A,
+                "phi": self.track.source.phi,
+                "D": self.track.source.D,
+                "T_dec": self.track.source.T_dec
+            })
+        elif self.point:
+            data.update({
+                "target_az_angle": self.point.target_az_angle,
+                "target_el_angle": self.point.target_el_angle
+            })
+        return data
 
 class Command(CreateCommand):
     id: int
